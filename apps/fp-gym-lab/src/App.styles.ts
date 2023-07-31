005501214f0a63dc7900e32fbec0d31d1d3f8264
pimenta <joao@excelsea.de>
2023-07-31 19:09:59 +0100

    chore(fp-gym-lab): Trigger test

---
+++ apps/fp-gym-lab/src/App.styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`;
--- apps/fp-gym-lab/src/App.tsx
+++ apps/fp-gym-lab/src/App.tsx
@@ -4,20 +4,15 @@ import logo from './logo.svg';
 
 import './App.css';
 
+import { Button } from './App.styles';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>FP Gym Lab webapp ready to go!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>FP Gym Lab ready to GO!</p>
+        <Button>Touch me for some magic 😘</Button>
       </header>
     </div>
   );
