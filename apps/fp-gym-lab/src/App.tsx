5254b0bdd641d9382019edb929ad4295813175a9
pimenta <joao@excelsea.de>
2023-07-31 18:25:37 +0100

    chore(fp-gym-lab): Add react app base code

--- apps/fp-gym-lab/src/App.tsx
+++ apps/fp-gym-lab/src/App.tsx
@@ -9,9 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>FP Gym Lab webapp ready to go ...</p>
         <a
           className="App-link"
           href="https://reactjs.org"
